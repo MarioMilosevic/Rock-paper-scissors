409707376ca02398f3658919e7ebb9218cd4f113
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-24 14:20:52 +0100

    Finish game functionality

--- main.js
+++ main.js
@@ -1,76 +1,67 @@
 "use strict";
+
 const root = document.querySelector(".root");
 const startBtnHtml = `<button class="startBtn">Start game</button>`;
 root.innerHTML = startBtnHtml;
 const startBtn = document.querySelector(".startBtn");
-
+let winner = "";
 const gameHtml = `<div class="app">
- <header class="header">Rock, Paper, Scissors ?</header>
-<p class="rules">First to 5 wins !!!</p>
-<div class="players">
+  <header class="header">Rock, Paper, Scissors ?</header>
+  <p class="rules">First to 3 wins !!!</p>
+  <div class="players">
   <div class="playerDiv">
-    <div class="playerInfo">
-        <h1>Player</h1>
-      <h1>Score - <span id="playerScore">0</span></h1>
-    </div>
+  <div class="playerInfo">
+  <h1>Player</h1>
+  <h1>Score - <span id="playerScore">0</span></h1>
+  </div>
   </div>
   <div class="computerDiv">
-    <div class="computerInfo">
-        <h1><span id="computerScore">0</span> - Score</h1>
-      <h1>Computer</h1>
-    </div>
+  <div class="computerInfo">
+  <h1><span id="computerScore">0</span> - Score</h1>
+  <h1>Computer</h1>
   </div>
-</div>
-<div class="winner hidden">
-${startBtnHtml}
-</div>
-<div class="modalOverlay hidden"></div>
-<div class="options">
+  </div>
+  </div>
+  <div class="modal hidden">
+    <h1>${winner}</h1>
+  </div>
+  <div class="overlay hidden">
+    <button class="startNewBtn">Start new game</button>
+  </div>
+  <div class="options">
   <img src="img/rock.png" alt="rock" id="rock" />
   <img src="img/paper.png" alt="paper" id="paper" />
   <img src="img/scissors.png" alt="scissors" id="scissors" />
-</div>
-</div>`;
-
-
-
-startBtn.addEventListener("click", startGame);
+  </div>
+  </div>`;
 
 function startGame() {
+  console.log("radi");
   root.innerHTML = gameHtml;
   const app = document.querySelector(".app");
   const playerScore = document.querySelector("#playerScore");
   const computerScore = document.querySelector("#computerScore");
-  const winner = document.querySelector(".winner");
-  const modalOverlay = document.querySelector(".modalOverlay");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
   const rock = document.querySelector("#rock");
   const paper = document.querySelector("#paper");
   const scissors = document.querySelector("#scissors");
-
+  const startNewBtn = document.querySelector(".startNewBtn");
   const gameOptions = [rock, paper, scissors];
-  const maxScore = 2;
+  const maxScore = 3;
 
   let playerResult = 0;
   let computerResult = 0;
-  // rock.addEventListener("click", function () {
-  //   rock.style.backgroundColor = "lightblue";
-  // });
 
-  // paper.addEventListener("click", function () {
-  //   paper.style.backgroundColor = "lightsalmon";
-  // });
   let gameFinished = false;
 
   function game(e) {
     if (!gameFinished) {
-      // Get player choice with click
       const playerChoice = e.target;
       playerChoice.style.backgroundColor = "lightblue";
-      // Get computer choice
       const randomChoice = Math.floor(Math.random() * gameOptions.length);
       const computerChoice = gameOptions[randomChoice];
       setTimeout(() => {
-        console.log(computerChoice);
         computerChoice.style.backgroundColor = "salmon";
         if (playerChoice === rock && computerChoice === paper) {
           computerWins();
@@ -100,7 +91,7 @@ function startGame() {
       setTimeout(() => {
         playerChoice.style.backgroundColor = "transparent";
         computerChoice.style.backgroundColor = "transparent";
-      }, 2500);
+      }, 2000);
     }
   }
 
@@ -111,17 +102,19 @@ function startGame() {
     computerScore.textContent = computerResult;
   }
 
-  function declareWinner(win) {
-    setTimeout(() => {
-      // winner.textContent = `${win} won`;
-      winner.classList.remove("hidden");
-      modalOverlay.classList.remove("hidden");
-      gameFinished = true;
-    }, 1500);
-  }
-
   function playerWins() {
     playerResult++;
     playerScore.textContent = playerResult;
   }
+  function declareWinner(win) {
+    setTimeout(() => {
+      modal.textContent = `${win} won !`;
+      modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+      gameFinished = true;
+    }, 1500);
+  }
+  startNewBtn.addEventListener("click", startGame);
 }
+startBtn.addEventListener("click", startGame);
+
